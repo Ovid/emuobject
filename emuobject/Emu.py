cab7c0b36e871f0333a683a882003e56dc9240e3
Ovid <ovid@cpan.org>
2024-01-11 13:02:29 +0100

    Move exceptions into their own file

--- emuobject/Emu.py
+++ emuobject/Emu.py
@@ -1,36 +1,20 @@
 from abc import ABC, abstractclassmethod
 from termcolor import colored
+from emuobject.EmuExceptions import MissingAllowedFieldError, MissingForbiddenFieldError
 
 # Need to have definitions for the fields have required and optional fields
-
-
-class EmuError(Exception):
-    def __init__(self, message: str) -> None:
-        self.message = message
-
-    def __str__(self) -> str:
-        return colored(self.message, "red")
-
-
-class MissingAllowedFieldError(EmuError):
-    def __init__(self, field) -> None:
-        super().__init__(f"Field '{field}; is allowed, but not present")
-
-
-class MissingForbiddenFieldError(EmuError):
-    def __init__(self, field) -> None:
-        super().__init__(f"Field '{field}' was requested, but not allowed")
-
 
 class Emu(ABC):
     def __init__(self, fields: dict) -> None:
         self._fields = fields
 
     @abstractclassmethod
-    def _definitions(cls) -> dict:
+    def definitions(cls) -> dict:
+        """This should be overridden in a subclass to return a dictionary of field names and their types."""
         pass
 
     def get(self, field: str):
+        """Returns the value of the field, or raises an exception if the field is not allowed or not present."""
         if not self.has(field):
             if self.allows(field):
                 raise MissingAllowedFieldError(field)
@@ -38,7 +22,9 @@ class Emu(ABC):
         return self._fields[field]
 
     def allows(self, field: str) -> bool:
-        return field in self._definitions()
+        """Returns True if the field is allowed (e.g., it's defined in definitions method), False otherwise."""
+        return field in self.definitions()
 
-    def has(self, field) -> bool:
+    def has(self, field: str) -> bool:
+        """Returns True if the field is present in the object, False otherwise."""
         return field in self._fields
